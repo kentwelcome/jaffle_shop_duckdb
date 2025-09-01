b7244515bff6e2610b6f763050c941fdda84b8f4
Kent Huang <kent@infuseai.io>
2025-09-01 23:18:52 +0800

    [PR3] Test PR
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -16,7 +16,7 @@ renamed as (
         payment_method,
 
         -- `amount` is currently stored in cents, so we convert it to dollars
-        amount / 100 as amount
+        (amount / 100)::DECIMAL(10,2) amount
 
     from source
 
